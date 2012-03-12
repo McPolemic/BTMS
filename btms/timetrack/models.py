b0af56383739df1019358f601be1967c06741439
Adam Lukens <spawn968@gmail.com>
2012-03-12 01:32:27 -0400

    Fixed CO Number description in timetrack

--- btms/timetrack/models.py
+++ btms/timetrack/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 """A CO/Status Combination"""
 class Status(models.Model):
-    co_num = models.IntegerField()
+    co_num = models.IntegerField('CO Number')
     task_name = models.CharField(max_length=50)
     status_name = models.CharField(max_length=30)
 
