819a405ed1fb942f98bdc8204859af3caf755976
Adam Lukens <spawn968@gmail.com>
2012-03-12 01:26:19 -0400

    Beginning of models for timetrack

--- btms/timetrack/models.py
+++ btms/timetrack/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+"""A CO/Status Combination"""
+class Status(models.Model):
+    co_num = models.IntegerField()
+    task_name = models.CharField(max_length=50)
+    status_name = models.CharField(max_length=30)
+
+"""Task that happened in a workday"""
+class Task(models.Model):
+    user = models.CharField(max_length=30)
+    date = models.DateField()
+    status = models.ForeignKey(Status)
+    total_minutes = models.IntegerField()
+
+    def _get_hours(self):
+        return self.total_minutes/60
+
+    hours = property(_get_hours)
+    
+    def _get_minutes(self):
+        return self.total_minutes - (self.hours*60)
+
+    minutes = property(_get_minutes)
+
+    def frac_hours(self):
+        return round((float(self.total_minutes)/60), 1)
+    
+    def english_time(self):
+        return '%d:%02d' % (self.hours, self.minutes)
+
+    english_time.short_description = "Time"
+    frac_hours.short_description = "Hours (Fraction)"
+
+"""A MyTask is a Status tied to a user for quick use"""
+class MyTask(models.Model):
+    user = models.CharField(max_length=30)
+    status = models.ForeignKey(Status)
--- btms/timetrack/views.py
+++ btms/timetrack/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from models import Status,Task,MyTask
 
 def index(request):
+    tasks = Task.objects.filter(user = "adam")
+    my_tasks = MyTask.objects.filter(user = "adam")
     return render_to_response('timetrack/timesheet.html',
-                              {},
+                              {'tasks': tasks,
+                               'my_tasks': my_tasks},
                               context_instance=RequestContext(request))
     
