2f4f9567de454e8f91a9ee5dfebec5041eec2b2d
Adam Lukens <spawn968@gmail.com>
2012-04-03 01:25:04 -0400

    My Tasks added to timesheet.
    Fixes #3

--- btms/static/base.js
+++ btms/static/base.js
@@ -31,6 +31,17 @@ function findIDs()
         if (isKeyIn(status, status_ids) === -1)
             status_ids.push(status);
     });
+
+    // An empty timesheet has no input fields
+    if (date_ids == false)
+    {
+        $("span[id^=date]").each(function() {
+            var date = $(this).attr("id").split("_")[0];
+
+            if (isKeyIn(date, date_ids) === -1)
+                date_ids.push(date);
+        })
+    }
     
     var value = [date_ids, status_ids]
     return value;
@@ -53,12 +64,16 @@ function updateTotals()
         $("input[id^=" + date_ids[i] + "]").each(function() {
             var value = parseFloat($(this).val());
 
+            if (value == 0)
+            {
+                $(this).val("");
+            }
+
             if (!isNaN(value))
                 total += value;
         });
 
         $("span[id=" + date_ids[i] + "_total]").text(total);
-        console.log("Setting total for " + date_ids[i] + " to " + total);
         grandTotal += total;
     }
 
@@ -82,6 +97,59 @@ function updateTotals()
     $("span[id=taskNone_total]").text(grandTotal);
 }
 
+function addStatusRow(status_id)
+{
+    var ids = findIDs();
+    var date_ids = ids[0];
+    var status_ids = ids[1];
+
+    // Don't add if it's already on the timesheet
+    for(var i = 0; i < status_ids.length; i++)
+        if (status_ids[i] === "task" + status_id)
+        {
+            return;
+        }
+
+    //Grab info about status
+    var mytask_row = $("tr#mytask_" + status_id).children();
+    var co_num = mytask_row[1].innerHTML;
+    var status = mytask_row[2].innerHTML;
+    var role   = mytask_row[3].innerHTML;
+    var task_text;
+
+    if (role !== "") {
+        task_text = role + " - " + status;
+    } else {
+        task_text = status;
+    }
+
+    //Find the total line <tr> element
+    var total_row = $("td:contains(Total)").parent();
+
+    var date_fields = ""
+    for (var i = 0; i < date_ids.length; i++)
+    {
+        date_fields += '<td class="time_entry"> \
+                            <input type="text" id="' + date_ids[i] + '_task' + status_id + '" name="' + date_ids[i] + '_task' + status_id + '" /> \
+                        </td>'
+    }
+
+    var row = ' <tr> \
+                    <td>' + co_num + '</td> \
+                    <td>' + task_text + '</td> \
+                    ' + date_fields + ' \
+                    <td class="align_right"><span id="task' + status_id + '_total"></span></td> \
+                </tr>'
+    total_row.before(row);
+
+    //Rebind the event handlers
+    var textFields = $("input[type=text]");
+    textFields.change(function() {isPageChanged = true;});
+    textFields.change(updateTotals);
+
+    return row;
+}
+
 function unsavedChangesWarning(e) {
     // Only fire if the page has been changed
     if (isPageChanged)
@@ -93,6 +161,6 @@ $(document).ready(function() {
     updateTotals();
 
     var textFields = $("input[type=text]");
-    textFields.change(function() {isPageChanged = true; console.log("Page dirty!");});
+    textFields.change(function() {isPageChanged = true;});
     textFields.change(updateTotals);
 });
